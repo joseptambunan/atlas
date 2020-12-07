d940ff1ad1f4fd4aa72a0f680476c66340743bf4
joseptambunan <josep.tambunan7@gmail.com>
2020-12-08 00:35:31 +0700

    add latest script

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,12 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\User;
-use Modules\Master\Entities\MasterModules;
-use Modules\Setting\Entities\AccessModules;
-use Modules\Setting\Entities\UserModules;
-use Modules\Master\Entities\MasterAdjusters;
-use Modules\Master\Entities\MasterPositions;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,58 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
-        $position = new MasterPositions;
-        $position->position_name = "SuperAdmin";
-        $position->save();    
-
-        $adjuster = new MasterAdjusters;
-        $adjuster->name = "SuperAdmin";
-        $adjuster->position_id = $position->id;;
-        $adjuster->save();
-
-    	$user = new User;
-    	$user->name = "SuperAdmin";
-    	$user->email = "admin@email.com";
-    	$user->password = bcrypt("123<>");
-    	$user->created_at = date("Y-m-d H:i:s");
-    	$user->created_by = 1;
-        $user->adjuster_id = $adjuster->id;
-    	$user->save();
-
-        $master_modules = new MasterModules;
-        $master_modules->modules_name = "Master";
-        $master_modules->save();
-
-        $access_modules = new AccessModules;
-        $access_modules->modules_id = $master_modules->id;
-        $access_modules->created = true;
-        $access_modules->read = true;
-        $access_modules->update = true;
-        $access_modules->insert = true;
-        $access_modules->save();
-
-        $user_modules = new UserModules;
-        $user_modules->user_id = $user->id;
-        $user_modules->access_approval_id = $access_modules->id;
-        $user_modules->save();
-
-        $master_modules = new MasterModules;
-        $master_modules->modules_name = "Setting";
-        $master_modules->save();
-
-        $access_modules = new AccessModules;
-        $access_modules->modules_id = $master_modules->id;
-        $access_modules->created = true;
-        $access_modules->read = true;
-        $access_modules->update = true;
-        $access_modules->insert = true;
-        $access_modules->save();
-
-        $user_modules = new UserModules;
-        $user_modules->user_id = $user->id;
-        $user_modules->access_approval_id = $access_modules->id;
-        $user_modules->save();
-
+        // $this->call(UserSeeder::class);
     }
 }
---
+++ database/seeds/ManagerSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Modules\Master\Entities\MasterAdjusters;
+use Modules\Setting\Entities\AccessModules;
+use Modules\Setting\Entities\UserModules;
+use App\Jobs\SendEmailUser;
+
+class ManagerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $open_file = fopen("./public/manager.csv","r");
+       	while(! feof($open_file)){
+	    	$csv_line = fgetcsv($open_file);
+	    	if ( $csv_line != "" ){
+	    		$check_user = User::where("email",'like','%'.$csv_line[1].'%')->get();
+	    		if ( count($check_user) <=0 ){
+	    			$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+			        $charactersLength = strlen($characters);
+			        $randomString = '';
+			        for ($i = 0; $i < 6; $i++) {
+			            $randomString .= $characters[rand(0, $charactersLength - 1)];
+			        }
+			        $adjuster = new MasterAdjusters;
+			        $adjuster->nik = NULL;
+			        $adjuster->name = $csv_line[0];
+			        $adjuster->email = $csv_line[1];
+			        $adjuster->phone = NULL;
+			        $adjuster->position_id = 7;
+			        $adjuster->created_at = date("Y-m-d H:i:s");
+			        $adjuster->created_by = 1;
+			        $adjuster->save();
+
+			        $user = new User;
+			        $user->name = $csv_line[0];
+			        $user->email = $csv_line[1];
+			        $user->password = crypt($randomString,'$6$rounds=5000$saltatkas$'); 
+			        $user->created_at = date("Y-m-d H:i:s");
+			        $user->created_by = 1;
+			        $user->adjuster_id = $adjuster->id;
+			        $user->save();
+
+			        $user_module = new UserModules;
+			        $user_module->user_id = $user->id;
+			        $user_module->access_approval_id = 4;
+			        $user_module->created_at = date("Y-m-d H:i:s");
+			        $user_module->created_by = 1;
+			        $user_module->save();
+
+			        $user_module = new UserModules;
+			        $user_module->user_id = $user->id;
+			        $user_module->access_approval_id = 5;
+			        $user_module->created_at = date("Y-m-d H:i:s");
+			        $user_module->created_by = 1;
+			        $user_module->save();
+
+			        $user_ = User::find($user->id);
+        			SendEmailUser::dispatch($user_, $randomString);
+
+			        echo $csv_line[1].','.$randomString;
+			        echo "\n";
+	    		}
+	    	}
+	    }
+    }
+}
